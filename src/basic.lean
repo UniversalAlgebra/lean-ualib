d7aec8c1c1dd055c59d58a82f2a519d4c8d7579e
Siva Somayyajula <ssomayya@cs.cmu.edu>
2018-07-20 00:38:37 -0400

    finishing merge

--- src/basic.lean
+++ src/basic.lean
@@ -6,27 +6,24 @@ import data.function
 open seq (map) (all)
 open function (op) (restrict)
 
--- Definition/s 1.1
-
 structure signature :=
 mk :: (F : Sort*) (ρ : F → ℕ)
 
 section
 parameter (S : signature)
 
--- define the operations of the algebra
+-- Defines the interpretation of an algebra on the carrier α
 def algebra_on (α) :=
-Π (f : S.F), op α (S.ρ f)  
+Π (f : S.F), op α (S.ρ f)
 
-def algebra :=  -- defines the universe and the operations ???
+-- An algebra pairs a carrier with an interpretation
+def algebra :=
 psigma algebra_on
 
 instance alg_univ : has_coe_to_sort algebra :=
 ⟨_, psigma.fst⟩
 
--- ^^^^ These coersions are cool! vvvvv
-
-instance alg_interp : has_coe_to_fun algebra :=  -- Should we call this instance `alg_ops` ?
+instance alg_ops : has_coe_to_fun algebra :=
 ⟨_, psigma.snd⟩
 
 end
@@ -34,12 +31,10 @@ end
 section
 parameter {S : signature}
 
--- Definition/s 1.2
-
 def is_subalgebra {α} (A : algebra_on S α) {β : set α} (B : algebra_on S β) :=
-∀ f b, ↑(B f b) = restrict (A f) β b  -- this is nice
+∀ f b, ↑(B f b) = restrict (A f) β b
 
-def homomorphic {A B : algebra S} (h : A → B) := -- Should we call this homomorphism ???
+def homomorphic {A B : algebra S} (h : A → B) :=
 ∀ f a, h (A f a) = B f (map h a)
 
 end
