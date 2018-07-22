77ac453c88c44ad2d757ae1078c9246edf124ed8
williamdemeo <williamdemeo@gmail.com>
2018-07-22 13:49:55 -0600

    remove old code in this branch (dev)

--- src/term.lean
+++ src/term.lean
@@ -6,41 +6,11 @@ import order.bounded_lattice
 open seq (map) (reduce) (all)
 open lattice
 
-/- section
-
-universes u v
- 
- --parameters (X : Sort u) (S : signature.{v})
-
-inductive term : ℕ → Sort (max 1 u v)
-| nullary (f : S.F) : S.ρ f = 0 → term 0
-| word : X → term 0
-| lift {m n} : m < n → term m → term n
-| apply (f) {n} : seq (term n) (S.ρ f) → term (n + 1)
-
- 
-theorem reduce_max {α} [semilattice_sup_bot α] {n} (a : seq α n) : all a (≤ reduce (⊔) ⊥ a) :=
-begin
-  intro i,
-  induction n with m ih,
-  exact fin.elim0 i,
-  admit
-end
-
-def term_algebra : algebra_on S (psigma term) :=
-λ (f : S.F) t,
-let n := reduce (⊔) ⊥ (map psigma.fst t) in
-
-⟨n + 1, begin
-let t' : seq (term n) (S.ρ f) := sorry,
-exact term.apply f t',
-end⟩
- -/
-
 
 
 -- The next few structures are direct translations of Andrej Bauer's Coq code
 -- at https://gist.github.com/andrejbauer/3cc438ab38646516e5e9278fdb22022c
+
 structure OpSignature :=
 (operation : Type)
 (arity : operation → Type)
@@ -69,7 +39,8 @@ section free_is_free
 
 variables (S : OpSignature) (X : Type) (A : OpAlgebra S) (h : X → A.carrier)
 
--- I don't have the rest working yet  
+-- That's all I have working so far...
+
 /- def induced_map : (termtree S X) → A.carrier
 | (generator x) := h x
 | (node o arg) := op A o (λ i, induced_map (arg i))
