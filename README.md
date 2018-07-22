41d239d8f1918b1d4f5b63e4cf72b0bdea9ba883
williamdemeo <williamdemeo@gmail.com>
2018-07-22 13:48:46 -0600

    Begin translating Andrej's Coq version of the term tree and free algebra

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # lean-ualib
 
 Library of Lean code for Universal Algebra
+
+## Installation
+
+Use of this library requires (at least) the following steps:
+
+1. clone this repository with, e.g.,
+
+   ```sh
+   git clone git@github.com:UniversalAlgebra/lean-ualib.git
+   ```
+
+   OR
+
+   ```sh
+   git clone https://github.com/UniversalAlgebra/lean-ualib.git
+   ```
+
+2. Change into the lean-ualib directory and run `leanpkg build`
+
+   ```sh
+   cd lean-ualib
+   leanpkg build
+   ```
+
+## Issues
+
+If you have a comment, question, suggestion, or problem related to this repository, please [open a new issue](https://github.com/UniversalAlgebra/lean-ualib/issues/new).
+
+## License
+
+This repository and its contents are released under and covered by the [Apache License, v 2.0](https://raw.githubusercontent.com/UniversalAlgebra/lean-ualib/master/LICENSE). Please read the [LICENSE file]((https://raw.githubusercontent.com/UniversalAlgebra/lean-ualib/master/LICENSE).
--- src/term.lean
+++ src/term.lean
@@ -6,10 +6,11 @@ import order.bounded_lattice
 open seq (map) (reduce) (all)
 open lattice
 
-section
+/- section
 
 universes u v
-parameters (X : Sort u) (S : signature.{v})
+ 
+ --parameters (X : Sort u) (S : signature.{v})
 
 inductive term : ℕ → Sort (max 1 u v)
 | nullary (f : S.F) : S.ρ f = 0 → term 0
@@ -17,7 +18,7 @@ inductive term : ℕ → Sort (max 1 u v)
 | lift {m n} : m < n → term m → term n
 | apply (f) {n} : seq (term n) (S.ρ f) → term (n + 1)
 
-
+ 
 theorem reduce_max {α} [semilattice_sup_bot α] {n} (a : seq α n) : all a (≤ reduce (⊔) ⊥ a) :=
 begin
   intro i,
@@ -34,5 +35,43 @@ let n := reduce (⊔) ⊥ (map psigma.fst t) in
 let t' : seq (term n) (S.ρ f) := sorry,
 exact term.apply f t',
 end⟩
+ -/
 
-end
+
+
+-- The next few structures are direct translations of Andrej Bauer's Coq code
+-- at https://gist.github.com/andrejbauer/3cc438ab38646516e5e9278fdb22022c
+structure OpSignature :=
+(operation : Type)
+(arity : operation → Type)
+
+structure OpAlgebra (S : OpSignature) :=
+(carrier : Type)
+(op : ∀ o : S.operation, ((S.arity o) → carrier) → carrier)
+
+structure Hom {S : OpSignature} (A : OpAlgebra S) (B : OpAlgebra S) :=
+(map : A.carrier → B.carrier) -- the underlying map
+(op_commute : ∀ (o : S.operation) (args : S.arity o → A.carrier),
+map (A.op o args) = B.op o (λ x, map (args x)))
+
+inductive termtree (S : OpSignature) (X : Type) : Type
+| generator : X → termtree
+| node : ∀ (o : S.operation), (S.arity o → termtree) → termtree
+
+#check termtree
+
+def Free (S : OpSignature) (X : Type) : OpAlgebra S :=
+OpAlgebra.mk (termtree S X) termtree.node
+
+#print Free
+
+section free_is_free
+
+variables (S : OpSignature) (X : Type) (A : OpAlgebra S) (h : X → A.carrier)
+
+-- I don't have the rest working yet  
+/- def induced_map : (termtree S X) → A.carrier
+| (generator x) := h x
+| (node o arg) := op A o (λ i, induced_map (arg i))
+ -/
+
