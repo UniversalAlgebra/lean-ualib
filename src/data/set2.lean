fe012fb51502148766793edeae345f826ef36371
Siva Somayyajula <ssomayya@cs.cmu.edu>
2018-07-23 17:00:30 -0400

    working on free algebra

--- src/data/set2.lean
+++ src/data/set2.lean
@@ -1,3 +1,6 @@
+instance set_type {α} : has_coe_to_sort (set α) :=
+⟨_, subtype⟩
+
 namespace set
 
 def sIntersection {α} (s : set (set α)) : set α := λ t, ∀ a ∈ s, t ∈ a
@@ -10,5 +13,8 @@ prefix `⋃ᵢ`:110 := iUnion
 
 def eq {α} {s₁ s₂ : set α} : s₁ ⊆ s₂ → s₂ ⊆ s₁ → s₁ = s₂ :=
 λ g h, funext (λ x, propext (iff.intro (@g x) (@h x)))
+
+def image_in {α β} (f : α → β) (s : set β) :=
+∀ x, f x ∈ s
 
 end set
