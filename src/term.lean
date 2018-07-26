d611e23c370ac8c7bc38e2d82cbef1e31e97bf33
williamdemeo <williamdemeo@gmail.com>
2018-07-26 02:01:59 -0600

    improvements

--- src/term.lean
+++ src/term.lean
@@ -1,60 +1,69 @@
 import basic
 
-import data.seq
-import order.bounded_lattice
-
-open seq (map) (reduce) (all)
-open lattice
-
-
 -- The next few structures are direct translations of Andrej Bauer's Coq code
 -- at https://gist.github.com/andrejbauer/3cc438ab38646516e5e9278fdb22022c
 
+
+section
 structure OpSignature :=
-(operation : Type)
-(arity : operation → Type)
+(op_symbol : Type*)
+(arity : op_symbol → Type*)
 
 structure OpAlgebra (S : OpSignature) :=
-(carrier : Type)
-(op : ∀ o : S.operation, ((S.arity o) → carrier) → carrier)
+(carrier : Type*)
+(op : ∀ f : S.op_symbol, ((S.arity f) → carrier) → carrier)
 
 structure Hom {S : OpSignature} (A : OpAlgebra S) (B : OpAlgebra S) :=
 (map : A.carrier → B.carrier) -- the underlying map
-(op_commute : ∀ (o : S.operation) (args : S.arity o → A.carrier),
-map (A.op o args) = B.op o (λ x, map (args x)))
+(op_commute : ∀ (f : S.op_symbol) (args : S.arity f → A.carrier),
+map (A.op f args) = B.op f (λ x, map (args x)))
 
-inductive term {S : OpSignature} (X : Type) : Type
-| gen : X → term
-| node : ∀ (o : S.operation), (S.arity o → term) → term
 
-#check term
+section terms
+  parameters (S : OpSignature) (X : Type*)
 
-def Free (S : OpSignature) (X : Type) : OpAlgebra S := 
-⟨@term S X, @term.node S X⟩ -- ⟨ carrier , operations ⟩ 
+  inductive term
+  | gen    : X → term
+  | node (f)  : --∀ (f : S.op_symbol), 
+  (S.arity f → term) → term
 
-#print Free
+  #check term
 
-section free_is_free
-variables (S : OpSignature) (A : OpAlgebra S) 
-variables (X : Type) (h : X → A.carrier) 
+  def free : OpAlgebra S := 
+  ⟨term, term.node⟩ -- ⟨ carrier , operations ⟩ 
 
-def induced_map : (@term S X) → A.carrier
-| (term.gen x) := h x
-| (term.node o args) :=  
-  (A.op o) (λ (i : S.arity o), induced_map (args i))
+end terms
 
--- Theorem (Existence): There is a homomorphism from Free S X into every 
---                      algebra of the given signature.
-theorem induced_hom : Hom (Free S X) A :=
-⟨induced_map S A X h,          -- the function
-  (assume (o: S.operation)     -- proof that the function preserves structure
-  (args : S.arity o → (Free S X).carrier), rfl)⟩  
+section outer
+
+  parameters {S : OpSignature} (A : OpAlgebra S) (X : Type*) 
+
+  open term
+  def term_ := @term S X  
+  def free_ := free S X  
+  def gen_ := @gen S X
+
+  section inner
+  parameter (h : X → A.carrier) 
+
+  def induced_map : free_.1 → A.1
+  | (gen .(S) x) := h x
+  | (node f a) :=  A.op f (λ (i : S.arity f), induced_map (a i))
+
+  -- Theorem (Existence): There is a homomorphism from Free S X into every 
+  --                      algebra of the given signature.
+  theorem induced_hom : Hom (free_) A :=
+  ⟨induced_map S A X h,          -- the function
+  (assume (o: S.op_symbol)     -- proof that the function preserves structure
+  (args : S.arity o → free_.carrier), rfl)⟩  
+
+  end inner
 
 -- Theorem (Uniqueness): The homomorphism in the previous theorem is unique.
-theorem from_free_unique (f g : Hom (Free S X) A) :
-(∀ (x : X), f.map (term.gen x) = g.map (term.gen x)) → 
-  ∀ (t : @term S X), f.map t = g.map t := 
-assume h1: (∀ (x : X), (f.map (term.gen x) = g.map (term.gen x))),
+theorem from_free_unique (f g : Hom (free_) A) :
+(∀ (x : X), f.map (gen_ x) = g.map (gen_ x)) → 
+  ∀ (t : term_), f.map t = g.map t := 
+assume h1: (∀ (x : X), (f.map (gen_ x) = g.map (gen_ x))),
 assume (t : @term S X), 
 show (f.map t = g.map t), from
 begin 
@@ -68,6 +77,6 @@ begin
   }
 end
 
+end outer
 
-
-end free_is_free
+end
