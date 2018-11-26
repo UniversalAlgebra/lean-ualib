064d092ff5ab78be252c9b7fc4fe0e48cf9ea55a
williamdemeo <williamdemeo@gmail.com>
2018-11-25 17:04:40 -0700

    add proof: equalizer of homs is a subalgebra

--- src/birkhoff.lean
+++ src/birkhoff.lean
@@ -7,46 +7,63 @@
         -`Type u` is syntactic sugar for `Sort (u+1)`
           (`Prop = Type -1` and `Sort 1 = Type 0`)
 -/
+-- import basic
+import basic
+import subuniverse 
+import data.set
 
-universe s -- where operation (s)ymbols live
-universe u -- where structure (u)niverses (i.e. carriers) live
-universe a -- where (a)rities live
+section
 
-section 
+  universe s -- where operation (s)ymbols live
+  universe u -- where structure (u)niverses (i.e. carriers) live
+  universe a -- where (a)rities live
 
   parameter υ : Sort u  -- A carrier type
   parameter τ : Sort u  -- Another carrier type
 
   -- Operation on the carrier υ (of arity α) 
-  def op (υ : Sort u) (α : Sort a) := (α → υ) → υ  
+--  def op (υ : Sort u) (α : Sort a) := (α → υ) → υ  
 
   -- Restricted Operation on the carrier υ (of arity α) 
   def op_restricted {υ : Sort u} (r : υ → Prop) (α : Sort a) := (α → (υ → Prop)) → (υ → Prop) 
 
-  -- i-th projection
-  def π {υ : Sort u} (α : Sort a) (i) : op υ α := λ x, x i
+   -- i-th projection
+   --def π {υ : Sort u} (α : Sort a) (i) : op υ α := λ x, x i
 
   -- Signature
   -- F : a set of operation symbols
   -- ρ : returns the arity of a given operation symbol
-  structure signature := 
-  mk :: (F : Sort s) (ρ : F → Sort a)
+--  structure signature := 
+--  mk :: (F : Sort s) (ρ : F → Sort a)
   
   -- The type of interpretations of operation symbols.
-  def algebra_on (υ : Sort u) (S : signature) := 
-  Π (f : S.F) , op υ (S.ρ f)
+--  def algebra_on (υ : Sort u) (S : signature) := Π (f : S.F) , op υ (S.ρ f)
   -- It's called `algebra_on` since an algebra is fully specified by its
-  -- Cayley (operation) tables. An inhabitant of `algebra_on` assigns to 
-  -- each op symbol f : F, of arity `α = S.ρ f`, a function of type (α → υ) → υ.
+  -- (Cayley) operation tables. An inhabitant of `algebra_on` assigns to 
+  -- each op symbol f : F, of arity `α = S.ρ f`, an interpretation of f, 
+  -- that is, a function of type (α → υ) → υ.
   
-  def homomorphic {υ : Sort u} {τ : Sort u} {S : signature} (A : algebra_on υ S) (B : algebra_on τ S) 
-  (h : υ → τ) : Prop := 
-  ∀ (f : S.F) (tuple : (S.ρ f) → υ), h (A f tuple) = (B f) (h ∘ tuple)
+--  def homomorphic {υ : Sort u} {τ : Sort u} {S : signature} 
+--  (A : algebra_on υ S) (B : algebra_on τ S) (h : υ → τ) : Prop := 
+--  ∀ (f : S.F) (tuple : (S.ρ f) → υ), h (A f tuple) = (B f) (h ∘ tuple)
 
-  def is_subuniverse {υ : Sort u} {S : signature} (A : algebra_on υ S) : (υ → Prop) → Prop :=
-  λ B, ∀ f, ∀ (tuple : S.ρ f → υ), (∀ x, B (tuple x)) → B (A f tuple)
-  -- Note: B z is true iff z ∈ B, so the line B (a x) → B (A f a) 
-  -- means: (a x) ∈ B  → (A f a) ∈ B.
+--parameters {α : Type*} {S : signature} (A : algebra S) (B : algebra S) {I ζ : Type} {R : I → set α} 
+--section
+parameters {α : Type*} {β : Type*} {S : signature} (A : algebra_on S α) 
+parameters (B : algebra_on S β) {I ζ : Type} {R : I → set α} 
+open set
+open subuniverse
+  -- parameter {S : signature} 
+  -- parameters {A : algebra_on S} {B : algebra_on τ S}
+  
+  -- def Sub (β : set α) : Prop :=
+  -- ∀ f (a : S.ρ f → α), (∀ x, a x ∈ β) → A f a ∈ β
+
+  -- def is_subuniverse {υ : Sort u} {S : signature} (A : algebra_on υ S) : (υ → Prop) → Prop :=
+/-   def is_subuniverse (B : υ → Prop) : Prop :=
+  ∀ f (tuple : S.ρ f → υ), (∀ x, B (tuple x)) → (A f tuple) ∈ B
+ -/  -- Note: z ∈ B is syntactic sugar for B z, so the line B (a x) → B (A f a) 
+  -- is understood to mean: (a x) ∈ B → (A f a) ∈ B.
 
   -- Elementary Facts ------------------------------------------
 
@@ -55,59 +72,77 @@ section
      Let $E(f,g) = \{ a \in A : f(a) = g(a) \}$ (the \defin{equalizer} of $f$ and $g$). 
      1. $E(f,g)$ is a subuniverse of $\alg{A}$.
      2. If $X \subseteq A$ and $X$ generates $\alg{A}$ and $\restr{f}{X} = \restr{g}{X}$, then $f = g$. 
-     3. If $\alg{A}, \alg{B}$ are finite and $X$ generates $\alg{A}$, then $|\!\Hom{\alg{A},\alg{B}}| \leq |B|^{|X|}$.
+     3. If $\mathbf A$, $\mathbf B$ are finite algebras and $X$ generates $\mathbf A$, then 
+        $|Hom(\mathbf A, \mathbf B)| \leq |B|^{|X|}$.
   -/
-
-  parameter {S : signature} 
-  parameters {A : algebra_on υ S} {B : algebra_on τ S}
-  
   -- equalizer for generic functions
-  def equalizer (h : υ → τ) (g : υ → τ) : υ → Prop := λ (x : υ), h x = g x 
-
-  -- equalizer for homomorphisms
-  def equalizer_of_homs (h : υ → τ) (g : υ → τ) 
-  (hh : homomorphic A B h) (hg : homomorphic A B g) : υ → Prop := 
-  λ (ai : υ), h ai = g ai 
-
-  -- 1. The equalizer $E(f,g)$ is a subuniverse of $\alg{A}$.
-  lemma equalizer_is_subuniverse
-  (h : υ → τ) (g : υ → τ) (hh : homomorphic A B h)  (hg : homomorphic A B g) : 
-  is_subuniverse A (equalizer h g) := 
-    assume (f : S.F) (tuple : S.ρ f → υ),
-    assume (h₁ : ∀ i, (equalizer h g) (tuple i)),
-    have h₂ : h ∘ tuple = g ∘ tuple, from funext h₁, 
-      calc h (A f tuple) = (B f) (h ∘ tuple): hh f tuple
-                     ... = (B f) (g ∘ tuple): congr_arg (B f) h₂ 
-                     ... = g (A f tuple) : by rw (hg f tuple)
-
-  lemma equalizer_of_homs_is_subuniverse (h : υ → τ) (g : υ → τ) 
-  (hh : homomorphic A B h) (hg : homomorphic A B g) : 
-  is_subuniverse A (equalizer_of_homs h g hh hg) := 
-    assume (f : S.F) (tuple : S.ρ f → υ),
-    assume (h₁ : ∀ i, (equalizer_of_homs h g hh hg) (tuple i)),
-    have h ∘ tuple = g ∘ tuple, from funext h₁, 
-    calc h (A f tuple) = (B f) (h ∘ tuple): hh f tuple
-                   ... = (B f) (g ∘ tuple): congr_arg (B f) this 
-                   ... = g (A f tuple) : by rw (hg f tuple)
-
-/- Here's a "paper-and-pencil" proof of equalizer_is_subuniverse:
-
-  Let $\rho$ be the similarity type of $\alg{A}$ and $\alg{B}$, and 
-  $f$ a (say, $n$-ary) operation symbol in $\rho$. Then, 
-  for every tuple $(a_1, \dots, a_n) \in E(h,g)^n$,
-  \begin{align*}
-    h(f^{\alg{A}}(a_1, \dots, a_n)) &= f^{\alg{B}}(h(a_1), \dots, h(a_n))\\
-                                    &= f^{\alg{B}}(g(a_1), \dots, g(a_n))
-                                     = g(f^{\alg{A}}(a_1, \dots, a_n)).
-  \end{align*}
-  Therefore, $E(f,g)$ is closed under $p$.  Since $p$ was arbitrary, 
-  $E(f,g)$ is closed under all operations in $\rho$ and is thus a 
-  subuniverse of $\alg{A}$.
--/
+  def equalizer (h : α → β) (g : α → β) : set α := λ (x : α), h x = g x 
 
 
---     2. If $X \subseteq A$ and $X$ generates $\alg{A}$ and $\restr{f}{X} = \restr{g}{X}$, then $f = g$. 
+  def hom (h : α → β) :=
+  ∀ f a, h (A f a) = B f (h ∘ a)
 
+  def equalizer_of_homs (h : α → β) (g : α → β) 
+  (hh : hom h) (hg : hom g) : set α := 
+  λ a, h a = g a 
+
+variables (h : α → β) (g : α → β)  (hh : hom h)  (hg : hom g) 
+#check equalizer
+#check @Sub
+
+  -- 1. The equalizer $E(f,g)$ is a subuniverse of $\mathbf A$.
+  lemma sub_equalizer (h : α → β) (g : α → β) (hh : hom h)  (hg : hom g) : 
+  Sub A (equalizer_of_homs h g hh hg) := 
+  assume f a (h₁: ∀ x, a x ∈ equalizer_of_homs h g hh hg),
+  show A f a ∈ (equalizer_of_homs h g hh hg),  from 
+    have h₂ : h ∘ a = g ∘ a, from funext h₁, 
+    show h (A f a) = g (A f a), from 
+      calc h (A f a) = (B f) (h ∘ a): hh f a
+                 ... = (B f) (g ∘ a): congr_arg (B f) h₂ 
+                 ... = g (A f a) : eq.symm (hg f a)
+  
+
+
+  -- 2. If X ⊆ A, if Sg(X) = \mathbf A, and if f x = g x for all x ∈ X, then f = g. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/- inductive term
+| var     : X → term
+| app (f) : (S.ρ f → term) → term
+
+def free : algebra S :=
+⟨term, term.app⟩
+ -/
+-- One way to make this SEGFAULT lol
+/- inductive subuniverse (X : υ) (S: signature) : Type
+| var : subuniverse 
+| app (f : S.F) (a : S.ρ f → υ) : (∀ i, subuniverse (a i)) → Y (A f a)
+
+theorem sg_inductive : Sg X = Y :=
+have h : Y ∈ Sub, from sorry,
+have l : Sg X ⊆ Y, from sorry,
+have r : Y ⊆ Sg X, from sorry,
+subset.antisymm l r
+def is_subuniverse' {υ : Sort u} {S : signature} (A : algebra_on υ S) : (υ → Prop) → Prop :=
+  λ B, ∀ f, ∀ (tuple : S.ρ f → υ), (∀ x, B (tuple x)) → B (A f tuple)
+  
 
 def generates (X : υ → Prop) : Prop := ∀ (SX : υ → Prop), --if B contains X and is a subalgebra of A, then B = A
 ∀ x, (X x → (SX x)) →  is_subuniverse A SX → is_subuniverse SX (λ z, true)
@@ -115,7 +150,7 @@ def generates (X : υ → Prop) : Prop := ∀ (SX : υ → Prop), --if B contain
 lemma homs_determined_on_gens (h : υ → τ) (g : υ → τ)
 (hh : @homomorphic S A B h) (hg : @homomorphic S A B g) (X : υ → Prop) : 
 (is_generating X A) → (∀ x, h x = g x) → h = g := sorry
- /- 
+ -/ /- 
   Suppose the subset $X \subseteq A$ generates $\alg{A}$ and suppose
   $\restr{f}{X} = \restr{g}{X}$.
   Fix an arbitrary element $a\in A$.  We show $f(a) = g(a)$.
